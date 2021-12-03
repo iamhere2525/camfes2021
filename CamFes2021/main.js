4989bd58c9ec3ea9025127e5669950eceb96192d
iamhere2525 <89884741+iamhere2525@users.noreply.github.com>
2021-12-03 12:54:16 +0900

    Update main.js

--- CamFes2021/main.js
+++ CamFes2021/main.js
@@ -3,26 +3,125 @@ const app = new Vue({
   data: {
     tabNum: 0,
     correctImage: -1,
-    titles: ["開会式", "CM", "音楽祭"],
+    titles: ["開会式","尼子の部屋1","ライブペイント","イントロ","音楽祭","Booo!踊ってみた","メインステージCM1","ゲームブースCM1","ギネス","メインブースCM2","マリカー","ダンスステージ","尼子の部屋2","英語禁止","カラオケ","ゲームブースCM2","Adorer fashion in Kobe","スマブラ","閉会式"],
     questions: [
       {
         question: "開会式のキーワードを打ってね",
         input: "",
-        correct: "あ",
+        correct: "開会式",
       },
       {
-        question: "CMのキーワードを打ってね",
+        question: "尼子の部屋1のキーワードを打ってね",
         input: "",
-        correct: "CM",
+        correct: "尼子1",
+      },
+      {
+        question: "ライブペイントのキーワードを打ってね",
+        input: "",
+        correct: "ライブペイント",
+      },
+      {
+        question: "イントロのキーワードを打ってね",
+        input: "",
+        correct: "イントロ",
       },
       {
         question: "音楽祭のキーワードを打ってね",
         input: "",
         correct: "音楽祭",
       },
+      {
+        question: "Booo!踊ってみたのキーワードを打ってね",
+        input: "",
+        correct: "Booo!",
+      },
+      {
+        question: "メインステージCM1のキーワードを打ってね",
+        input: "",
+        correct: "メイン1",
+      },
+      {
+        question: "ゲームブースCM1のキーワードを打ってね",
+        input: "",
+        correct: "ゲーム1",
+      },
+      {
+        question: "ギネスのキーワードを打ってね",
+        input: "",
+        correct: "ギネス",
+      },
+      {
+        question: "メインブースCM2のキーワードを打ってね",
+        input: "",
+        correct: "メイン2",
+      },
+      {
+        question: "マリカーのキーワードを打ってね",
+        input: "",
+        correct: "マリカー",
+      },
+      {
+        question: "ダンスステージのキーワードを打ってね",
+        input: "",
+        correct: "ダンスステージ",
+      },
+      {
+        question: "尼子の部屋2のキーワードを打ってね",
+        input: "",
+        correct: "尼子1",
+      },
+      {
+        question: "英語禁止のキーワードを打ってね",
+        input: "",
+        correct: "英語禁止",
+      },
+      {
+        question: "カラオケのキーワードを打ってね",
+        input: "",
+        correct: "カラオケ",
+      },
+      {
+        question: "ゲームブースCM2のキーワードを打ってね",
+        input: "",
+        correct: "ゲーム2",
+      },
+      {
+        question: "Adorer fashion in Kobeのキーワードを打ってね",
+        input: "",
+        correct: "Kobe",
+      },
+      {
+        question: "スマブラのキーワードを打ってね",
+        input: "",
+        correct: "スマブラ",
+      },
+      {
+        question: "閉会式のキーワードを打ってね",
+        input: "",
+        correct: "閉会式",
+      },
+
     ],
     images: [
-      "10_12_15_8_14_659.png",
+      "yoshinagababy.jpg",
+      "iidababy.jpg",
+      "fuzimotobaby.jpg",
+      "kobayashibaby.jpg",
+      "momokobaby.jpg",
+      "unenobaby.jpg",
+      "yoshinobaby.jpg",
+      "yamadababy.jpg",
+      "miyoshibaby.jpg",
+      "yoshinagababy2.jpg",
+      "ayaibaby.jpg",
+      "muraobaby.jpg",
+      "kakehibaby.jpg",
+      "momokoobaby.jpg",
+      "kawasakibaby.jpg",
+      "hondababy.jpg",
+      "minatomotobaby.jpg",
+      "ashidababy.jpg",
+      "yamadababy2.jpg",
     ]
   },
   computed: {
@@ -38,9 +137,8 @@ const app = new Vue({
       if (this.questions[index].input === this.questions[index].correct) {
         this.correctImage = index;
       } else {
-        alert("不正解");
+        alert("違うよ！");
       }
     }
   }
 })
-  
